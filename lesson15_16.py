dd9e5019f5305fd1da0a0075385c4279c4c25d39
Kobzar Roman <kobzarroman@mail.ru>
2025-10-23 11:22:48 +1000

    lesson16

---
+++ lesson15_16.py
@@ -0,0 +1,38 @@
+# ООП 15 Магические методы. Методы __str__ и __repr__. (Dunder methods)
+from queue import PriorityQueue
+
+
+class Lion:
+    def __init__(self, name):
+        self.name = name
+
+    # добавляет информацию для разработчиков, если __str__ не присвоен то в консоле также отображается эта же информация
+    def __repr__(self):
+        return f'The object Lion - {self.name}'
+
+    # добавляет информацию для отображения в консоле (для пользователя)
+    def __str__(self):
+        return f'Lion - {self.name}'
+
+a = Lion('Anton')
+print(repr(a))
+
+b = Lion('Simba')
+print(b)
+
+class Otrezok:
+    def __init__(self, x1, x2):
+        self.x1 = x1
+        self.x2 = x2
+
+# такая запись вызовет магический метод __abc__
+    def __len__(self):
+        return abs(self)
+
+    def __abs__(self):
+        return abs(self.x2 - self.x1)
+
+f = Otrezok(55, 5)
+print(len(f))
+print(Otrezok.__dict__)
+print(Lion.__dict__)
